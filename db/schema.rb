63bd76c01eb48ee9b967cb6ac5e31dd508a76e33
dgt <digitaria@riseup.net>
2019-07-15 19:00:21 +0200

    WIP just one db migration - remove separate migration for changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_10_190538) do
-
-  create_table "archive_versions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
-    t.integer "archive_id"
-    t.integer "version"
-    t.string "filename"
-    t.string "state", limit: 10, default: "pending"
-    t.integer "created_by_id"
-    t.integer "updated_by_id"
-    t.integer "group_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["archive_id"], name: "index_archive_versions_on_archive_id"
-  end
+ActiveRecord::Schema.define(version: 201907147160218) do
 
   create_table "asset_versions", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "asset_id"
@@ -163,9 +150,7 @@ ActiveRecord::Schema.define(version: 2019_07_10_190538) do
   create_table "group_archives", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "filename"
     t.integer "state", default: 0
-    t.integer "version", default: 0
     t.integer "created_by_id"
-    t.integer "updated_by_id"
     t.integer "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
